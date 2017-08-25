3a51ea142016390d2f71c94a3cb240833c33fa0b
Ryan Hausler <rmh1995@gmail.com>
2017-08-25 00:30:30 -0400

    housekeeping

--- Code/DNA2RNA.R
+++ Code/DNA2RNA.R
@@ -2,10 +2,10 @@
 
 library(DECIPHER)
 
-p_dna  <- readLines("~/Desktop/NP.fasta") # FASTA is 5' DNA
-p_rna  <- gsub(pattern = "T", replace = "U", x = tx) # Change to RNA
-writeLines(tx2, con="~/Desktop/NP_RNA.fasta") # Write out to new file
-p_rna = readRNAStringSet('~/Desktop/NP_RNA.fasta') # Read in RNA FASTA
+p_dna  <- readLines("~/Desktop/NS.fasta") # FASTA is 5' DNA
+p_rna  <- gsub(pattern = "T", replace = "U", x = p_dna) # Change to RNA
+writeLines(p_rna, con="~/Desktop/NS_RNA.fasta") # Write out to new file
+p_rna = readRNAStringSet('~/Desktop/NS_RNA.fasta') # Read in RNA FASTA
 n_rna = reverseComplement(p_rna) # Change to 3' RNA
 
 align = AlignSeqs(n_rna[1:5000], processors = 4) # Align 3' RNA strands
@@ -17,22 +17,18 @@ cutoffs <- sort(unique(c(seq(0, 1, 0.01),
                          round(quantile(d, seq(0, 1, 0.01), na.rm=TRUE), 5))))
 dimnames(d) <- NULL
 # Clusters similar seqs
-# Makes phylogenetic tree
-# Each row is a seq. Each column is a cutoff.
-# An index is what group each seq in
 suppressWarnings(guideTree <- IdClusters(d,
                                          method="UPGMA",
                                          cutoff=cutoffs,
                                          verbose=FALSE))
 guideTree <- guideTree
-# Holds the cutoff at each split
 lastSplit <- numeric(dim(guideTree)[1])
 weights <- numeric(dim(guideTree)[1])
 numGroups <- 1
 
 for (i in (dim(guideTree)[2] - 1):1) {
   if (length(unique(guideTree[, i])) > numGroups) {
-    if (numGroups==1) { # root of tree
+    if (numGroups==1) {
       lastSplit[] <- cutoffs[i]
     } else {
       for (j in unique(guideTree[, i + 1])) {
@@ -51,4 +47,4 @@ weights <- ifelse(weights==0, 1, weights)
 weights <- weights/mean(weights)
 
 states = PredictDBN(align, processors = 4, weight = weights)
-save(states, file='~/Desktop/NP_states.RData')
+save(states, file='~/Desktop/NS_states.RData')
